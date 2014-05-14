6307ac15cf191f379e7e3077bebcfa3752ead7dd
Justin York <justin@genealogysystems.com>
2014-05-14 15:02:08 +0000

    test calc weight for wrd

---
+++ test/calc-weight.js
@@ -0,0 +1,111 @@
+var expect = require('chai').expect,
+    wrds = require('./wrds.js'),
+    traverse = require('./../lib/fs-traversal.js')({});
+    
+describe('_calcWeight', function(){
+
+  describe('wrd', function(){
+  
+    it('base person', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['base person']});
+      expect(weight).to.equal(1);
+    });
+    
+    it('parents', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['parents']});
+      expect(weight).to.equal(2);
+    });
+    
+    it('siblings', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['siblings']});
+      expect(weight).to.be.closeTo(2.72, .01);
+    });
+    
+    it('grandparents', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['grandparents']});
+      expect(weight).to.equal(3);
+    });
+    
+    it('children', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['children']});
+      expect(weight).to.be.closeTo(3.52, .01);
+    });
+    
+    it('great grandparents', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['great grandparents']});
+      expect(weight).to.equal(4);
+    });
+    
+    it('spouse', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['spouse']});
+      expect(weight).to.be.closeTo(4.14, .01);
+    });
+    
+    it('2g grandparents', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['2g grandparents']});
+      expect(weight).to.equal(5);
+    });
+    
+    it('grandchildren', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['grandchildren']});
+      expect(weight).to.be.closeTo(5.28, .01);
+    });
+    
+    it('aunts and uncles', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['aunts and uncles']});
+      expect(weight).to.be.closeTo(5.44, .01);
+    });
+    
+    it('3g grandparents', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['3g grandparents']});
+      expect(weight).to.equal(6);
+    });
+    
+    it('4g grandparents', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['4g grandparents']});
+      expect(weight).to.equal(7);
+    });
+    
+    it('great grandchildren', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['great grandchildren']});
+      expect(weight).to.be.closeTo(8.8, .01);
+    });
+    
+    it('1st cousins', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['1st cousins']});
+      expect(weight).to.be.closeTo(3.52, .01);
+    });
+    
+    it('5g grandparents', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['5g grandparents']});
+      expect(weight).to.equal(8);
+    });
+    
+    it('great aunts and uncles', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['great aunts and uncles']});
+      expect(weight).to.be.closeTo(8.15, .01);
+    });
+    
+    it('parents-in-law', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['parents-in-law']});
+      expect(weight).to.be.closeTo(8.27, .01);
+    });
+    
+    it('6g grandparents', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['6g grandparents']});
+      expect(weight).to.equal(9);
+    });
+    
+    it('nieces and nephews', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['nieces and nephews']});
+      expect(weight).to.be.closeTo(9.57, .01);
+    });
+    
+    it('7g grandparents', function(){     
+      var weight = traverse._calcWeight({wrd: wrds['7g grandparents']});
+      expect(weight).to.equal(10);
+    });
+  
+  });
+
+});
---
+++ test/wrds.js
@@ -0,0 +1,143 @@
+module.exports = {
+
+  'base person': {
+    g: 0,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  'parents': {
+    g: 1,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  'siblings': {
+    g: 0,
+    c: 1,
+    m: 0,
+    up: false
+  },
+  
+  'grandparents': {
+    g: 2,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  'children': {
+    g: -1,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  'great grandparents': {
+    g: 3,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  'spouse': {
+    g: 0,
+    c: 0,
+    m: 1,
+    up: false
+  },
+  
+  '2g grandparents': {
+    g: 4,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  'grandchildren': {
+    g: -2,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  'aunts and uncles': {
+    g: 1,
+    c: 1,
+    m: 0,
+    up: false
+  },
+  
+  '3g grandparents': {
+    g: 5,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  '4g grandparents': {
+    g: 6,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  'great grandchildren': {
+    g: -4,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  '1st cousins': {
+    g: -1,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  '5g grandparents': {
+    g: 7,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  'great aunts and uncles': {
+    g: 2,
+    c: 1,
+    m: 0,
+    up: false
+  },
+  
+  'parents-in-law': {
+    g: 1,
+    c: 0,
+    m: 1,
+    up: false
+  },
+  
+  '6g grandparents': {
+    g: 8,
+    c: 0,
+    m: 0,
+    up: false
+  },
+  
+  'nieces and nephews': {
+    g: -1,
+    c: 1,
+    m: 0,
+    up: false
+  },
+  
+  '7g grandparents': {
+    g: 9,
+    c: 0,
+    m: 0,
+    up: false
+  }
+
+};
