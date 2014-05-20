21d11ba9b8a47b0fa1d684cae68b66bd7b46338a
Justin York <justin@genealogysystems.com>
2014-05-20 20:06:42 +0000

    More relationshipTo tests; base person special case

--- test/relationshipTo.js
+++ test/relationshipTo.js
@@ -4,15 +4,88 @@ var expect = require('chai').expect,
     FSTraversal = require('./../lib/fs-traversal.js');
     
 describe('relationshipTo', function(){
+
+  it('should return "yourself"', function(){
+    var str = FSTraversal(sdk)._relationshipTo(genPath());
+    expect(str).to.equal('yourself');
+  });
   
-  it('should return parent', function(){    
-    var str = FSTraversal(sdk)._relationshipTo(['1', 'father','2']);
+  it('parent', function(){    
+    var str = FSTraversal(sdk)._relationshipTo(genPath('father'));
     expect(str).to.equal('parent');
   });
+  
+  it('child', function(){    
+    var str = FSTraversal(sdk)._relationshipTo(genPath('child'));
+    expect(str).to.equal('child');
+  });
+  
+  it('spouse', function(){    
+    var str = FSTraversal(sdk)._relationshipTo(genPath('spouse'));
+    expect(str).to.equal('spouse');
+  });
+  
+  it('grandparent', function(){    
+    var str = FSTraversal(sdk)._relationshipTo(genPath('father', 'father'));
+    expect(str).to.equal('grandparent');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('father', 'mother'));
+    expect(str).to.equal('grandparent');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('mother', 'father'));
+    expect(str).to.equal('grandparent');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('mother', 'mother'));
+    expect(str).to.equal('grandparent');
+  });
+  
+  it('sibling', function(){    
+    var str = FSTraversal(sdk)._relationshipTo(genPath('father', 'child'));
+    expect(str).to.equal('sibling');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('mother', 'child'));
+    expect(str).to.equal('sibling');
+  });
+  
+  it('grandchild', function(){    
+    var str = FSTraversal(sdk)._relationshipTo(genPath('child', 'child'));
+    expect(str).to.equal('grandchild');
+  });
+  
+  it('great-grandparent', function(){    
+    var str = FSTraversal(sdk)._relationshipTo(genPath('father', 'father', 'father'));
+    expect(str).to.equal('great-grandparent');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('father', 'father', 'mother'));
+    expect(str).to.equal('great-grandparent');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('father', 'mother', 'father'));
+    expect(str).to.equal('great-grandparent');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('father', 'mother', 'mother'));
+    expect(str).to.equal('great-grandparent');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('mother', 'father', 'father'));
+    expect(str).to.equal('great-grandparent');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('mother', 'father', 'mother'));
+    expect(str).to.equal('great-grandparent');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('mother', 'mother', 'father'));
+    expect(str).to.equal('great-grandparent');
+    var str = FSTraversal(sdk)._relationshipTo(genPath('mother', 'mother', 'mother'));
+    expect(str).to.equal('great-grandparent');
+  });
+  
+  it('great-grandchild', function(){    
+    var str = FSTraversal(sdk)._relationshipTo(genPath('child', 'child', 'child'));
+    expect(str).to.equal('great-grandchild');
+  });
 
-  it('should return great uncle', function(){
-    var str = FSTraversal(sdk)._relationshipTo(['1', 'father','2', 'father','3', 'father','4', 'child','5', 'child','6']);
+  it('great uncle\'s child', function(){
+    var str = FSTraversal(sdk)._relationshipTo(genPath('father', 'father', 'father', 'child', 'child')); 
     expect(str).to.equal('great-uncle\'s child');
   });
   
 });
+
+/**
+ * Generate relationship paths in the syntax fs-traversal expects
+ */
+function genPath(){
+  var path = [''];
+  for(var i = 0; i < arguments.length; i++){
+    path.push(arguments[i], '');
+  }
+  return path;
+};
