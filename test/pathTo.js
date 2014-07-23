ade7c96d5f5912e0bfd1d72431c77793c88df459
Justin York <justin@genealogysystems.com>
2014-07-23 20:51:50 +0000

    New format for fetched paths

--- test/pathTo.js
+++ test/pathTo.js
@@ -17,7 +17,7 @@ describe('pathTo', function(){
       .done(function(){
         var path = traversal.pathTo('1');
         expect(path).to.have.length(1);
-        expect(path[0].id).to.equal('1');
+        expect(path[0].person.id).to.equal('1');
         done();
       });
   });
@@ -27,56 +27,20 @@ describe('pathTo', function(){
       .order('distance')
       .traverse('1')
       .done(function(){
-        expect(traversal.pathTo('2')).to.deep.equal([{id:'1',name:'base person'},'spouse',{id:'2',name:'spouse'}]);
-        expect(traversal.pathTo('3')).to.deep.equal([{id:'1',name:'base person'},'father',{id:'3',name:'father'}]);
-        expect(traversal.pathTo('4')).to.deep.equal([{id:'1',name:'base person'},'mother',{id:'4',name:'mother'}]);
-        expect(traversal.pathTo('5')).to.deep.equal([{id:'1',name:'base person'},'child',{id:'5',name:'child'}]);
+        expect(traversal.pathTo('2')).to.deep.equal([
+          {rel:'start',person:{id:'1',name:'base person'}},
+          {rel:'spouse',person:{id:'2',name:'spouse'}}]);
+        expect(traversal.pathTo('3')).to.deep.equal([
+          {rel:'start',person:{id:'1',name:'base person'}},
+          {rel:'father',person:{id:'3',name:'father'}}]);
+        expect(traversal.pathTo('4')).to.deep.equal([
+          {rel:'start',person:{id:'1',name:'base person'}},
+          {rel:'mother',person:{id:'4',name:'mother'}}]);
+        expect(traversal.pathTo('5')).to.deep.equal([
+          {rel:'start',person:{id:'1',name:'base person'}},
+          {rel:'child',person:{id:'5',name:'child'}}]);
         done();
       });
   });
-  
-  it('more complicated example', function(){
-    var getPerson = function(id, name){
-      return {
-        getPrimaryPerson: function(){
-          return {
-            id: id,
-            display: {
-              name: name
-            }
-          }
-        }
-      };
-    };
-    var traversal = FSTraversal(sdk);
-    traversal._fetched['fmcsm'] = {
-      path: ['start','father','f','mother','fm','child','fmc','spouse','fmcs','mother','fmcsm']
-    };
-    traversal._visited['start'] = getPerson('start', 'Start Person');
-    traversal._visited['f'] = getPerson('f', 'Father');
-    traversal._visited['fm'] = getPerson('fm', 'Fathers Mother');
-    traversal._visited['fmc'] = getPerson('fmc', 'Fathers Mothers Child');
-    traversal._visited['fmcs'] = getPerson('fmcs', 'Fathers Mothers Childs Spouse');
-    traversal._visited['fmcsm'] = getPerson('fmcsm', 'Fathers Mothers Childs Spouses Mother');
 
-    var path = traversal.pathTo('fmcsm');
-    expect(path).to.have.length(11);
-    expect(path[0]).to.have.property('id', 'start');
-    expect(path[0]).to.have.deep.property('display.name', 'Start Person');
-    expect(path[1]).to.equal('father');
-    expect(path[2]).to.have.property('id', 'f');
-    expect(path[2]).to.have.deep.property('display.name', 'Father');
-    expect(path[3]).to.equal('mother');
-    expect(path[4]).to.have.property('id', 'fm');
-    expect(path[4]).to.have.deep.property('display.name', 'Fathers Mother');
-    expect(path[5]).to.equal('child');
-    expect(path[6]).to.have.property('id', 'fmc');
-    expect(path[6]).to.have.deep.property('display.name', 'Fathers Mothers Child');
-    expect(path[7]).to.equal('spouse');
-    expect(path[8]).to.have.property('id', 'fmcs');
-    expect(path[8]).to.have.deep.property('display.name', 'Fathers Mothers Childs Spouse');
-    expect(path[9]).to.equal('mother');
-    expect(path[10]).to.have.property('id', 'fmcsm');
-    expect(path[10]).to.have.deep.property('display.name', 'Fathers Mothers Childs Spouses Mother');
-  });
 });
