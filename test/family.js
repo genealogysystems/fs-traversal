2d4e5bf20d6d1e6ee7c19f4b89891c182305e2f2
Justin <justincyork@gmail.com>
2015-04-03 22:42:25 +0000

    test family thoroughly

--- test/family.js
+++ test/family.js
@@ -1,37 +1,62 @@
 var expect = require('chai').expect,
-    graph = require('./graphs/simple.js'),
-    sdk = require('./lib/mock-sdk.js')(graph),
+    simpleGraph = require('./graphs/simple.js'),
+    largeGraph = require('./graphs/large.js'),
+    simpleSdk = require('./lib/mock-sdk.js')(simpleGraph),
+    largeSdk = require('./lib/mock-sdk.js')(largeGraph),
     FSTraversal = require('../lib/fs-traversal.js');
     
-describe.only('family callback', function(){
+describe('family callback', function(){
   
   it('simple', function(done){
-    var count = 0;
-    FSTraversal(sdk)
+    var familyIds = [];
+    FSTraversal(simpleSdk)
       .order('distance')
-      .person(function(person){
-        console.log(person.id);
-      })
       .family(function(wife, husband, children){
-        count++;
-        console.log(wife.id, husband.id, children.length, children.map(function(child){
+        familyIds.push([wife.id,husband.id].concat(children.map(function(child){
           return child.id;
-        }).join(':'));
-        if(wife.id === '2'){
-          expect(husband.id).to.equal('1');
-          expect(children).to.have.length(1);
-          expect(children[0].id).to.equal('5');
-        } else if(wife.id === '4') {
-          expect(husband.id).to.equal('3');
-          expect(children).to.have.length(1);
-          expect(children[0].id).to.equal('1');
-        } else {
-          expect(false).to.equal.true;
-        }
+        })).join(':'));
       })
       .traverse('1')
       .done(function(){
-        expect(count).to.equal(2);
+        expect(familyIds).to.have.length(2);
+        expect(familyIds).to.have.members([
+          '4:3:1',
+          '2:1:5'
+        ]);
+        done();
+      });
+  });
+  
+  it('large', function(done){
+    var familyIds = [];
+    FSTraversal(largeSdk)
+      .order('distance')
+      .family(function(wife, husband, children){
+        var familyId = [wife.id,husband.id].concat(children.map(function(child){
+          return child.id;
+        })).join(':');
+        familyIds.push(familyId);
+      })
+      .traverse('1')
+      .done(function(){
+        expect(familyIds).to.have.length(15);
+        expect(familyIds).to.have.members([
+          '2:1:5',
+          '4:3:29:1',
+          '33:5:34',
+          '20:21:22:2',
+          '29:30:31',
+          '7:6:8:4',
+          '25:24:27:21',
+          '9:8:10',
+          '14:13:18:6',
+          '15:16:14',
+          '37:36:35',
+          '27:26:28',
+          '11:10:12',
+          '18:17:19',
+          '39:38:37'
+        ]);
         done();
       });
   });
