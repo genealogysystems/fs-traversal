e01a1000ed9c37413ebf5e85cb6e944a26535232
Justin <justincyork@gmail.com>
2015-04-16 22:34:08 +0000

    calculate wrd values on-demand; #29

--- test/traversal-wrd.js
+++ test/traversal-wrd.js
@@ -1,6 +1,8 @@
 var expect = require('chai').expect,
-    graph = require('./graphs/simple.js'),
-    sdk = require('./lib/mock-sdk.js')(graph),
+    simpleGraph = require('./graphs/simple.js'),
+    simpleSDK = require('./lib/mock-sdk.js')(simpleGraph),
+    largeGraph = require('./graphs/large.js'),
+    largeSDK = require('./lib/mock-sdk.js')(largeGraph),
     FSTraversal = require('./../lib/fs-traversal.js');
     
 describe('traversal - wrd', function(){
@@ -13,12 +15,9 @@ describe('traversal - wrd', function(){
       4: 2,
       5: 3.52
     };
-    var visitedPersons = [],
-        marriageCount = 0,
-        childCount = 0,
-        parentCount = 0;
+    var personCount = 0;
         
-    FSTraversal(sdk)
+    FSTraversal(simpleSDK)
       .order('wrd')
       .wrd({
         gPositive: 1,
@@ -27,36 +26,78 @@ describe('traversal - wrd', function(){
         m: 1.42
       })
       .person(function(person){
-        visitedPersons.push(person);
+        personCount++;
         expect(this._calcWeight(this._fetched[person.id])).to.be.closeTo(weights[person.id], .01);
-      })
-      .marriage(function(wife, husband, marriage){
-        expect(visitedPersons).to.deep.include.members([wife]);
-        expect(visitedPersons).to.deep.include.members([husband]);
-        expect(marriage).to.exist;
-        expect(graph.marriages).to.deep.include.members([{husband: husband.id, wife: wife.id}]);
-        marriageCount++;
-      })
-      .child(function(child, mother, father, relationship){
-        expect(visitedPersons).to.deep.include.members([child]);
-        expect(visitedPersons).to.deep.include.members([mother]);
-        expect(visitedPersons).to.deep.include.members([father]);
-        expect(relationship).to.exist;
-        expect(graph.childofs).to.deep.include.members([{child: child.id, father: father.id, mother: mother.id}]);
-        childCount++;
-      })
-      .parent(function(parent, child){
-        expect(visitedPersons).to.deep.include.members([parent]);
-        expect(visitedPersons).to.deep.include.members([child]);
-        parentCount++;
       })
       .traverse('1')
       .done(function(){
-        expect(visitedPersons).to.have.length(5);
-        expect(childCount).to.equal(2);
-        expect(parentCount).to.equal(4);
+        expect(personCount).to.have.equal(5);
         done();
       });
   });
+  
+  it('large', function(done){
+    var weights = {
+      1: 1,
+      3: 2,
+      4: 2,
+      29: 2.718281828459045,
+      6: 3,
+      7: 3,
+      5: 3.52,
+      13: 4,
+      14: 4,
+      2: 4.137120440251392,
+      15: 5,
+      16: 5,
+      34: 5.28,
+      8: 5.43656365691809,
+      10: 7.3890560989306495,
+      18: 8.154845485377136,
+      20: 8.274240880502784,
+      21: 8.274240880502784,
+      30: 11.245859314881844,
+      22: 11.245859314881844,
+      24: 12.411361320754176,
+      25: 12.411361320754176,
+      33: 14.5626639496849,
+      19: 14.778112197861299,
+      35: 21.843995924527352,
+      36: 14.5626639496849,
+      37: 14.5626639496849,
+      38: 4.137120440251392,
+      39: 4.137120440251392,
+      9: 22.49171862976369,
+      27: 22.49171862976369,
+      31: 26.009477468235886,
+      11: 30.569415021050204,
+      28: 30.569415021050204,
+      17: 33.73757794464554,
+      23: 46.52547443978919,
+      12: 70.70108996962058,
+      26: 93.05094887957839,
+      32: 107.6043408740967
+    };
+    var personCount = 0;
+        
+    FSTraversal(largeSDK)
+      .order('wrd')
+      .wrd({
+        gPositive: 1,
+        gNegative: 1.76,
+        c: 1,
+        m: 1.42
+      })
+      .person(function(person){
+        personCount++;
+        //console.log('%s: %d,', person.id, this._calcWeight(this._fetched[person.id]));
+        expect(this._calcWeight(this._fetched[person.id])).to.be.closeTo(weights[person.id], .01);
+      })
+      .traverse('1')
+      .done(function(){
+        expect(personCount).to.have.equal(39);
+        done();
+      });
+  })
   
 });
